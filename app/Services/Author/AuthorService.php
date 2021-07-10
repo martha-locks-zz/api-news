074e39c93c242990c0e32776b501d6b167fce5ad
Martha Locks <marthalocks@gmail.com>
2021-07-10 17:44:45 -0300

    Create Services

---
+++ app/Services/Author/AuthorService.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Services\AbstractService;
+
+/**
+ * class AuthorService
+ * @package App\Services\AuthorService
+ */
+class AuthorService extends AbstractService
+{
+}
---
+++ app/Services/ImageNews/ImageNewsService.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\ImageNews;
+
+use App\Services\AbstractService;
+use App\Repositories\ImageNews\ImageNewsRepository;
+
+/**
+ * Class ImageNewsService
+ * @package App\Services\ImageNews
+ */
+class ImageNewsService extends AbstractService
+{
+    private ImageNewsRepository $repository;
+
+    public function __constructor(ImageNewsRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * @param int $newsId
+     * @return array
+     */
+    public function findByNews(int $newsId): array
+    {
+        return $this->repository->findByNews($newsId);
+    }
+
+    /**
+     * @param int $newsId
+     * @return bool 
+     */
+    public function deleteByNews(int $newsId): bool
+    {
+        return $this->repository->deleteByNews($newsId);
+    }
+}
