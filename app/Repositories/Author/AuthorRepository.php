a14f958ee2629af59eb9c511a4c29e513c9b45ff
Martha Locks <marthalocks@gmail.com>
2021-07-10 16:21:36 -0300

    Create Repositories

---
+++ app/Repositories/Author/AuthorRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repositories\Author;
+
+use App\Repositories\AbstractRepository;
+
+/**
+ * class AuthorRepository
+ * @package App\Repositories\Author
+ */
+
+class AuthorRepository extends AbstractRepository
+{
+
+}
---
+++ app/Repositories/ImageNews/ImageNewsRepository.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repositories\ImageNews;
+
+use App\Repositories\AbstractRepository;
+
+/**
+ * Class ImageNewsRepository
+ * @package App\Repositories\ImageNews
+ */
+
+class ImageNewsRepository extends AbstractRepository
+{
+    /**
+     * @param int $newsId
+     * @return array
+     */
+
+    public function findByNews(int $newsId): array
+    {
+        return $this->model::where('news_id', $newsId)
+            ->get()
+            ->toArray();
+    }
+
+    /**
+     * @param int $newsId
+     * @return bool 
+     */
+    
+    public function deleteByNews(int $newsId): bool
+    {
+        $result = $this->model::where('news_id', $newsId)
+            ->delete();
+        
+            return $result ? true : false;
+    }
+}
